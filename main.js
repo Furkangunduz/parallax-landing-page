a2294e1326e0de9ebe058734ec2ecf8bd0ec8b81
Furkan <furkan.gundz00@gmail.com>
2022-09-05 15:36:54 +0300

    .

---
+++ main.js
@@ -0,0 +1,43 @@
+// IMAGE SLIDER //
+const slideBtns = document.querySelectorAll('[data-slideBtn]');
+const slideContainer = document.querySelector('[data-slideContainer]');
+const slides = [...document.querySelectorAll('[data-slide]')];
+let currentIndex = 0;
+let isMoving = false;
+
+// btn handle function
+function handleSlideBtnClick(e) {
+    if (isMoving) return;
+    isMoving = true;
+    e.currentTarget.id === "prev"
+        ? currentIndex--
+        : currentIndex++;
+
+    slideContainer.style.transform = `translateX(-${currentIndex * slides[0].clientWidth}px)`
+    removeDisabledAttribute(slideBtns);
+    currentIndex === 0 && addDisabledAttribute([slideBtns[0]]);
+
+}
+
+// remove/add attirubte function
+const removeDisabledAttribute = (els) => els.forEach(el => el.removeAttribute('disabled'));
+const addDisabledAttribute = (els) => els.forEach(el => el.setAttribute('disabled', 'true'));
+
+// event listeners
+slideBtns.forEach(btn => btn.addEventListener('click', handleSlideBtnClick));
+
+
+// transition end event
+slideContainer.addEventListener('transitionend', () => isMoving = false);
+
+// disable image drag events
+document.querySelectorAll('[data-slide] img').forEach(img => img.ondragstart = () => false);
+
+// intersection observer for slider
+const slideObserver = new IntersectionObserver((slide) => {
+    if (slide[0].isIntersecting) {
+        addDisabledAttribute([slideBtns[1]]);
+    }
+}, { threshold: .75 });
+
+slideObserver.observe(slides[slides.length - 1]);
